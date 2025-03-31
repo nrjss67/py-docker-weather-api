d6c24248593d174b36bcd3c88af48641b810819b
nrjss67 <extazzzyprofit@gmail.com>
2025-03-31 16:33:03 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import requests
+import os
 
+
+def get_weather() -> None:
+    API_KEY = os.getenv("API_KEY")
+    r = requests.get(f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q=Paris")
+    print(r.json())
 
 if __name__ == "__main__":
     get_weather()
