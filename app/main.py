20103da54017e075f22a38ffe23eede5c453b3f6
nrjss67 <extazzzyprofit@gmail.com>
2025-03-31 16:49:49 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import os
 
 
 def get_weather() -> None:
-    API_KEY = os.getenv("API_KEY")
-    r = requests.get(f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q=Paris")
-    print(r.json())
+    API_KEY = os.getenv("API_KEY") # noqa
+    if API_KEY: # noqa
+        r = requests.get(f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q=Paris") # noqa
+        print(r.json()) # noqa
 
 if __name__ == "__main__":
     get_weather()
